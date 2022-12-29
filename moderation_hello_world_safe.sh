250bfd91f604744adfdce2bc1330ff1910186596
Michael Hornstein <mdhornstein@gmail.com>
2022-12-28 23:22:50 -0800

    add moderation_hello_world_safe.sh

---
+++ moderation_hello_world_safe.sh
@@ -0,0 +1,5 @@
+curl https://api.openai.com/v1/moderations \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -d '{"input": "I like to eat broccoli."}'
